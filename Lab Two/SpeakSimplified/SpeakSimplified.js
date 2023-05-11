c0744345cbcf284a3b229408cb1b7d905278186d
AAzarudeen <azarudeenasraffali@gmail.com>
2023-05-11 18:01:09 +0530

    Two Labs

--- Lab Two/SpeakSimplified/SpeakSimplified.js
+++ Lab Two/SpeakSimplified/SpeakSimplified.js
@@ -1,14 +1,11 @@
-const input = document.getElementById("input")
-        const exclusion = document.getElementById("exclusion")
-        const strengthen = document.getElementById("strengthen")
-        const emphasize = document.getElementById("emphasize")
-        const result = document.getElementById("result")
-        input.addEventListener('keyup',()=>{
-            result.innerText = input.value
-        })
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+input.addEventListener("keyup", () => {
+  result.innerText = input.value;
+});
 
-        function addPrefix(prefix) {
-            if (input.value.length != 0) {
-                result.innerText = prefix+input.value
-            }
-        }
+function addPrefix(prefix) {
+  if (input.value.length != 0) {
+    result.innerText = prefix + input.value.toLowerCase();
+  }
+}
