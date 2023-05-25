5223483c13933ae1f85c1d3fe59c4859a382208c
AAzarudeen <azarudeenasraffali@gmail.com>
2023-05-25 10:29:57 +0530

    code updated

--- Lab One/calc/calculator.js
+++ Lab One/calc/calculator.js
@@ -1,6 +1,18 @@
 const input_box = document.getElementById("input_box")
-
 function input_fun(number) {
+    var symbol = ['+','-','*','/']
+    var str = input_box.textContent
+    if(number == 0 && str == "")
+        return
+    if(str == "" &&  symbol.includes(number)){
+        return
+    }
+    if(symbol.includes(str[str.length-1]) && symbol.includes(number)){
+        if(str[str.length-1] != number){
+            input_box.textContent = (str.slice(0,str.length-1)+number)
+        }
+        return
+    }
     input_box.textContent += number
 }
 
